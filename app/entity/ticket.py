28bd38c497bd83e0b36481c8151f9ee58d5b7d57
hallan-n <hallanneves01@gmail.com>
2023-12-20 18:43:14 -0300

    feat: add verify autor

--- app/entity/ticket.py
+++ app/entity/ticket.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel, validator
 
+
 class Ticket(BaseModel):
     id: int
+    author: int
     description: str
 
     @validator("description")
     def validate_description(cls, value):
-        if len(value):
+        if not len(value):
+            raise ValueError("")
+        return value
+
+    @validator("autor")
+    def validate_author(cls, value):
+        if not value:
             raise ValueError("")
         return value
