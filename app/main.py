242d8a0cac71f759ed178cd857e640bf6580ea5d
hallan-n <hallanneves01@gmail.com>
2023-12-19 22:21:24 -0300

    feat: Run app fastapi

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-print("Teste")
+from fastapi import FastAPI
+
+
+class App(FastAPI):
+    def __init__(self, *argS, **kwargs) -> None:
+        super().__init__(*argS, **kwargs)
+
+
+app = App()
+
+
+@app.get("/")
+async def entry():
+    return "Rodando"
