049ac1dcf5b024dbaa6f857399e83642e2d122f1
hallan-n <hallanneves01@gmail.com>
2023-12-23 02:58:49 -0300

    feat: add database

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,6 @@
 from app.entity.base import Base
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
 
 
 class Database:
