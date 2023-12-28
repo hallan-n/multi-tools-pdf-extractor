6f70e645334caf63fe5e386805e10ce18f58f721
hallan-n <hallanneves01@gmail.com>
2023-12-27 21:38:23 -0300

    feat: test db connection

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-import os
 from db.database import Database
+from entities.ticket_sql import TicketSQL
+import os
 
 
 class App(FastAPI):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
-    # def include_routers(self):
-    #     self.include_router()
 
 
 app = App()
@@ -16,6 +14,9 @@ app = App()
 
 @app.get("/")
 async def entry():
-    DB_URL = f'mysql+mysqlconnector://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}/{os.environ.get("DB_DATABASE")}'
-    db = Database(DB_URL)
-    return db
+    try:
+        DB_URL = f'mysql+mysqlconnector://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}/{os.environ.get("DB_DATABASE")}'
+        db = Database(DB_URL)
+        return "Conectou"
+    except:
+        return "Não conectou"
