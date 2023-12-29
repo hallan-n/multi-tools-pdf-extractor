1e18ee733576403361a74bc2480bbb50402d3b2e
hallan-n <hallanneves01@gmail.com>
2023-12-28 22:33:32 -0300

    refactor: refactor docker-compose/file

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM python:3.11
 
 WORKDIR /app
 
+ENV WATCHFILES_FORCE_POLLING=true
+
+RUN pip install --upgrade pip
+
 COPY requirements.txt /app/
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
-
-EXPOSE 8000
-
-CMD ["WATCHFILES_FORCE_POLLING=true", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- app/main.py
+++ app/main.py
@@ -3,17 +3,15 @@ from app.db.database import Database
 import os
 
 
-class App(FastAPI):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-
-app = App()
+app = FastAPI()
 
 
 @app.get("/")
 async def entry():
-    DB_URL = f'mysql+mysqlconnector://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}/{os.environ.get("DB_DATABASE")}'
-    db = Database(DB_URL)
-    db.is_connect()
-    return "Não conecto"
+    try:
+        DB_URL = f'mysql+mysqlconnector://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}/{os.environ.get("DB_DATABASE")}'
+        db = Database(DB_URL)
+        db.is_connect()
+        return "Rodando"
+    except:
+        return "Não conectou"
