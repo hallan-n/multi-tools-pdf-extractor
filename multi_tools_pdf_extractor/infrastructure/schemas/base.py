d394dcd9a2cb47efbcfbc2503bde69fd0e853e04
hallan-n <hallanneves01@gmail.com>
2024-03-14 22:15:16 -0300

    refactor: modify base schemas

--- multi_tools_pdf_extractor/infrastructure/schemas/base.py
+++ multi_tools_pdf_extractor/infrastructure/schemas/base.py
@@ -1,4 +1,8 @@
 from sqlalchemy.orm import declarative_base
+from .group_schema import Group
+from .pdf_schema import PDF
+from .quick_text_schema import QuickText
+from .ticket_schema import Ticket
 
 BaseModel = declarative_base()
 
