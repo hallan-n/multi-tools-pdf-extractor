f250d6085b25e5cc7bbbfc7be868ce01ae934014
hallan-n <hallanneves01@gmail.com>
2024-04-03 00:16:14 -0300

    feat: add get_metadatas method

--- app/infrastructure/api/routes/pdf_router.py
+++ app/infrastructure/api/routes/pdf_router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-from fastapi import UploadFile, File
 import base64
+from io import BytesIO
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from PyPDF2 import PdfReader, PdfWriter
 
 router = APIRouter()
 
@@ -9,11 +11,16 @@ router = APIRouter()
 @router.post("/pdf/base64")
 async def to_base64(files: list[UploadFile] = File(...)):
     base64_files = []
-    for file in files:
+    for index, file in enumerate(files, start=1):
         if file.filename.endswith(".pdf") or file.filename.endswith(".PDF"):
             contents = await file.read()
             base64_encoded = base64.b64encode(contents).decode("utf-8")
-            base64_files.append({"filename": file.filename, "base64": base64_encoded})
+            base64_files.append(
+                {
+                    "filename": f"file-{str(index).zfill(2)}.pdf",
+                    "base64": base64_encoded,
+                }
+            )
     return JSONResponse(content=base64_files)
 
 
@@ -33,11 +40,54 @@ async def to_base64(filename="file-", files: list[UploadFile] = File(...)):
     return renamed_files
 
 
-@router.post("/pdf")
-async def crack_pass():
-    pass
+@router.post("/pdf/pass")
+async def crack_pass(files: list[UploadFile] = File(...), password: str = "999"):
+    decrypted_pdfs = []
+
+    for index, file in enumerate(files, start=1):
+        if file.filename.lower().endswith(".pdf"):
+            try:
+                pdf_content = await file.read()
+                input_pdf = BytesIO(pdf_content)
+                reader = PdfReader(input_pdf)
+                if reader.is_encrypted:
+                    reader.decrypt(password)
+                    writer = PdfWriter()
+                    for page in reader.pages:
+                        writer.add_page(page)
+                    output_pdf = BytesIO()
+                    writer.write(output_pdf)
+                    output_pdf.seek(0)
+                    base64_content = base64.b64encode(output_pdf.getvalue())
+                else:
+                    output_pdf = input_pdf
+
+                decrypted_pdfs.append(
+                    {
+                        "filename": f"file-{str(index).zfill(2)}.pdf",
+                        "base64_content": base64_content.decode("utf-8"),
+                    }
+                )
+            except Exception as e:
+                raise HTTPException(
+                    status_code=500,
+                    detail=f"Erro ao processar o PDF '{file.filename}': {str(e)}",
+                )
+
+    return decrypted_pdfs
 
 
-@router.post("/pdf")
-async def get_metadatas():
-    pass
+@router.post("/pdf/meta")
+async def get_metadatas(files: list[UploadFile] = File(...)):
+    pdf_reader = PdfReader(files[0].file)
+
+    meta = {
+        "title": pdf_reader.metadata.title,
+        "author": pdf_reader.metadata.author,
+        "creator": pdf_reader.metadata.creator,
+        "producer": pdf_reader.metadata.producer,
+        "creation_date": pdf_reader.metadata.creation_date,
+        "modification_date": pdf_reader.metadata.modification_date,
+    }
+
+    return meta
